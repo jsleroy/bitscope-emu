5ae449449acd2b486e26a5df754a31ed8e6f64bb
Jean-Sebastien Leroy <jealer01@arm.com>
2015-08-05 08:32:41 +0200

    add register update

--- test.py
+++ test.py
@@ -32,48 +32,50 @@ class BitScope:
     )
 
     bytecode = {
-        0x00: "Reset the Virtual Machine. Print the Byte-Code number.",
-        0x3F: "Print the 8 character string identifying the revision.",
-        0x5B: "Clear R0. Usually commences byte entry.",
-        0x30: "Increment R0 by 0 and nibble swap.",
-        0x31: "Increment R0 by 1 and nibble swap.",
-        0x32: "Increment R0 by 2 and nibble swap.",
-        0x33: "Increment R0 by 3 and nibble swap.",
-        0x34: "Increment R0 by 4 and nibble swap.",
-        0x35: "Increment R0 by 5 and nibble swap.",
-        0x36: "Increment R0 by 6 and nibble swap.",
-        0x37: "Increment R0 by 7 and nibble swap.",
-        0x38: "Increment R0 by 8 and nibble swap.",
-        0x39: "Increment R0 by 9 and nibble swap.",
-        0x61: "Increment R0 by 10 and nibble swap.",
-        0x62: "Increment R0 by 11 and nibble swap.",
-        0x63: "Increment R0 by 12 and nibble swap.",
-        0x64: "Increment R0 by 13 and nibble swap.",
-        0x65: "Increment R0 by 14 and nibble swap.",
-        0x66: "Increment R0 by 15 and nibble swap.",
-        0x5D: "Swap nibbles in R0. Usually concludes byte entry.",
-        0x40: "Set Address Register R1.",
-        0x23: "Set Source Address Register R2.",
-        0x73: "Store byte R0 to register (R1).",
-        0x6C: "Load byte from register (R2) to R0.",
-        0x6E: "Increment Address Register R1.",
-        0x70: "Print register (R1).",
-        0x2B: "Increment register (R1).",
-        0x70: "Decrement register (R1).",
-        0x3C: "Capture Spock Counter to R9,R10.",
-        0x3E: "Program Spock Registers from R3...R7.",
-        0x54: "Trace until trigger + delay, then print Spock Counter.",
-        0x44: "Delay until trigger and Trace, then print Spock Counter.",
-        0x4C: "Trace Logic until trigger, then print Spock Counter.",
-        0x53: "Sample dump (CSV format, analogue & digital data).",
-        0x4D: "Mixed memory dump (Binary format, analogue & digital data).",
-        0x45: "Scan for Event, then print Spock Counter.",
-        0x41: "Analog memory dump (Binary format, analogue data).",
-        0x50: "Measure time Period.",
-        0x75: "Update RAM pointers R3,R4 to R9,R10.",
-        0x7C: "Transmit byte in R18 to POD IO-0.",
-        0x52: "Read EEPROM byte at address (R17) and print.",
-        0x57: "Write byte R16 to EEPROM address (R17).",
+        0x00: ("Reset the Virtual Machine. Print the Byte-Code number.", reset),
+        0x21: ("Soft VM reset.", reset),
+        0x23: ("Set Source Address Register R2.", None),
+        0x2B: ("Increment register (R1).", None),
+        0x30: ("Increment R0 by 0 and nibble swap.", reg_update),
+        0x31: ("Increment R0 by 1 and nibble swap.", reg_update),
+        0x32: ("Increment R0 by 2 and nibble swap.", reg_update),
+        0x33: ("Increment R0 by 3 and nibble swap.", reg_update),
+        0x34: ("Increment R0 by 4 and nibble swap.", reg_update),
+        0x35: ("Increment R0 by 5 and nibble swap.", reg_update),
+        0x36: ("Increment R0 by 6 and nibble swap.", reg_update),
+        0x37: ("Increment R0 by 7 and nibble swap.", reg_update),
+        0x38: ("Increment R0 by 8 and nibble swap.", reg_update),
+        0x39: ("Increment R0 by 9 and nibble swap.", reg_update),
+        0x3C: ("Capture Spock Counter to R9,R10.", None),
+        0x3E: ("Program Spock Registers from R3...R7.", None),
+        0x3F: ("Print the 8 character string identifying the revision.", None),
+        0x40: ("Set Address Register R1.", None),
+        0x41: ("Analog memory dump (Binary format, analogue data).", None),
+        0x44: ("Delay until trigger and Trace, then print Spock Counter.", None),
+        0x45: ("Scan for Event, then print Spock Counter.", None),
+        0x4C: ("Trace Logic until trigger, then print Spock Counter.", None),
+        0x4D: ("Mixed memory dump (Binary format, analogue & digital data).", None),
+        0x50: ("Measure time Period.", None),
+        0x52: ("Read EEPROM byte at address (R17) and print.", None),
+        0x53: ("Sample dump (CSV format, analogue & digital data).", None),
+        0x54: ("Trace until trigger + delay, then print Spock Counter.", None),
+        0x55: ("Configure device hardware.", None),
+        0x57: ("Write byte R16 to EEPROM address (R17).", None),
+        0x5B: ("Clear R0. Usually commences byte entry.", None),
+        0x5D: ("Swap nibbles in R0. Usually concludes byte entry.", None),
+        0x61: ("Increment R0 by 10 and nibble swap.", reg_update),
+        0x62: ("Increment R0 by 11 and nibble swap.", reg_update),
+        0x63: ("Increment R0 by 12 and nibble swap.", reg_update),
+        0x64: ("Increment R0 by 13 and nibble swap.", reg_update),
+        0x65: ("Increment R0 by 14 and nibble swap.", reg_update),
+        0x66: ("Increment R0 by 15 and nibble swap.", reg_update),
+        0x6C: ("Load byte from register (R2) to R0.", reg_update),
+        0x6E: ("Increment Address Register R1.", None),
+        0x70: ("Decrement register (R1).", None),
+        0x70: ("Print register (R1).", None),
+        0x73: ("Store byte R0 to register (R1).", None),
+        0x75: ("Update RAM pointers R3,R4 to R9,R10.", None),
+        0x7C: ("Transmit byte in R18 to POD IO-0.", None),
     }
 
     registers = (
@@ -103,20 +105,41 @@ class BitScope:
         "Period Pulse Count",
     )
 
-s = serial.Serial('/home/jean-sebastien/COM2')
+registers = [None] * len(BitScope.registers)
 
-registers = [0] * len(BitScope.registers)
+
+def reset():
+    registers = [0] * len(BitScope.registers)
+
+def version():
+    s.write('\r')
+    s.write(BitScope.versions[7])
+    s.write('\r')
+
+def reg_update(cmd):
+    register[0] += (cmd & 0xf)
+    register[0] = register[0] + 4;
+    print hex(register[0])
+
+
+s = serial.Serial('uart2')
 
 while True:
     char = s.read(1)
-    print hex(ord(char)),
-    try:
-        print bytecode[ord(char)]
-    except:
-        print '???'
-    s.write(char)
 
-    if char == '?':
-        s.write('\r')
-        s.write(BitScope.versions[7])
-        s.write('\r')
+    # Print received command
+    cmd = ord(char)
+    print hex(cmd),
+
+    # Get handler
+    f = bytecode[cmd][2]
+
+    if f is None:
+        print '???'
+    else:
+        print bytecode[ord(char)]
+
+    f(cmd)
+
+    # Send command back when it has been executed
+    s.write(char)
